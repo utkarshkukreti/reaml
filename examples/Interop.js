1459139c9457770c7e422b2a6163e455967e8b12
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2019-12-29 17:26:59 +0530

    fix React key warning in Interop example's js

--- examples/Interop.js
+++ examples/Interop.js
@@ -3,9 +3,11 @@ import { createElement as h } from 'react';
 
 export const Counter = ({ initial }) => {
   const [count, setCount] = React.useState(initial);
-  return h('div', {}, [
+  return h(
+    'div',
+    {},
     h('button', { onClick: () => setCount(count - 1) }, '-'),
     ` ${count} `,
     h('button', { onClick: () => setCount(count + 1) }, '+'),
-  ]);
+  );
 };
