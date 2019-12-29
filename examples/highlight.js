3194e2ac25ba60fba9d1adf4bae5e6534ba0bd74
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2019-12-29 16:51:29 +0530

    show relevant js file in examples if it exists

--- examples/highlight.js
+++ examples/highlight.js
@@ -1,7 +1,9 @@
 import hljs from 'highlight.js/lib/highlight';
 import ocaml from 'highlight.js/lib/languages/ocaml';
+import javascript from 'highlight.js/lib/languages/javascript';
 
 hljs.registerLanguage('ocaml', ocaml);
+hljs.registerLanguage('javascript', javascript);
 
 document.querySelectorAll('pre code').forEach(block => {
   hljs.highlightBlock(block);
