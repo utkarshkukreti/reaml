2f95df996724e7d0d4bcdfcd18581f1b559381ee
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2020-07-12 18:41:55 +0530

    don't even need a default value

--- src/Reaml_Core.ml
+++ src/Reaml_Core.ml
@@ -258,14 +258,14 @@ let component ?memo:(memo_ = false) ~(name : string) (fn : 'props functionCompon
 
 (* Create Recursive Component *)
 let recursiveComponent
-    ?(memo = false)
+    ?memo
     ~(name : string)
     (fn : 'props -> 'props functionComponent -> vnode)
     : 'props functionComponent
   =
   let make = ref (fun _ -> null) in
   let fn props = fn props !make in
-  make := component ~memo ~name fn;
+  make := component ?memo ~name fn;
   !make
 
 (* Portal *)
